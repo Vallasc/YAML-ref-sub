c6c2dee8416dd607fb8eff89c70801a506713b86
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-10-28 10:52:08 +0100

    feat: add implementation

--- src/main/java/dev/vallasc/App.java
+++ src/main/java/dev/vallasc/App.java
@@ -1,38 +1,97 @@
 package dev.vallasc;
 
-import com.sun.codemodel.JCodeModel;
 import org.jsonschema2pojo.*;
-import org.jsonschema2pojo.rules.RuleFactory;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws IOException, URISyntaxException {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        System.out.println("Hello World!");
 
-        JCodeModel codeModel = new JCodeModel();
-
-        URL source = new URI("http://google.com").toURL();
+        URL source = new URI("https://raw.githubusercontent.com/Vallasc/YAML-ref-sub/refs/heads/main/testfiles/main/main.yaml").toURL();
+        String outDir = "./target/generated/jsonschema";
 
         GenerationConfig config = new DefaultGenerationConfig() {
             @Override
-            public boolean isGenerateBuilders() { // set config option by overriding method
-                return true;
+            public boolean isGenerateBuilders() {
+                return false;
+            }
+
+            @Override
+            public SourceType getSourceType() {
+                return SourceType.YAMLSCHEMA;
+            }
+
+            @Override
+            public boolean isIncludeAdditionalProperties() {
+                return false;
+            }
+
+            @Override
+            public File getTargetDirectory() {
+                return new File(outDir);
+            }
+
+            @Override
+            public Iterator<URL> getSource() {
+                return Collections.singletonList(source).iterator();
             }
         };
 
-        SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
-        mapper.generate(codeModel, "ClassName", "com.example", source);
+        generateSources(config);
+        System.out.println(replaceRelativeRefs(source));
+    }
 
-        codeModel.build(Files.createTempDirectory("required").toFile());
+    public static void generateSources(GenerationConfig config) throws IOException {
+        Jsonschema2Pojo.generate(config, new NoopRuleLogger());
+    }
+
+    public static String replaceRelativeRefs(URL yamlFileUrl) throws IOException, URISyntaxException {
+        // Carica e processa il file YAML
+        InputStream inputStream = yamlFileUrl.openStream();
+        Yaml yaml = new Yaml();
+        Map<String, Object> yamlData = yaml.load(inputStream);
+
+        // Processa i riferimenti $ref
+        replaceRefs(yamlData, yamlFileUrl);
+
+        // Stampa il YAML modificato
+        DumperOptions options = new DumperOptions();
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        Yaml outputYaml = new Yaml(options);
+        return outputYaml.dump(yamlData);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void replaceRefs(Map<String, Object> node, URL mainYamlUrl) throws MalformedURLException, URISyntaxException {
+        if (node == null) return;
+
+        for (Map.Entry<String, Object> entry : node.entrySet()) {
+            if (entry.getValue() instanceof Map) {
+                Map<String, Object> childNode = (Map<String, Object>) entry.getValue();
+
+                // Controlla e sostituisce i riferimenti $ref
+                if (childNode.containsKey("$ref")) {
+                    String refPath = (String) childNode.get("$ref");
+
+                    // Se il riferimento è relativo, convertilo a uno statico
+                    URL resolvedUrl = new URL(mainYamlUrl, refPath); // Risolve il percorso relativo
+                    childNode.put("$ref", resolvedUrl.toString());
+                }
+                // Ricorsivamente processa i nodi figli
+                replaceRefs(childNode, mainYamlUrl);
+            }
+        }
     }
 }
